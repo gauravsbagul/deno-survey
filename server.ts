07dea67414f33112aeae55e3ef67e095d4849042
Gaurav Bagul <gauravsbagul@gmail.com>
2020-07-03 21:35:09 +0530

    added postman collection link

--- server.ts
+++ server.ts
@@ -32,3 +32,8 @@ function newFunction(
 // run cmd - denon run --allow-net --allow-write --allow-read --allow-plugin --allow-env --unstable server.ts
 
 //https://www.youtube.com/watch?v=TQUy8ENesGY&t=317s
+
+// Postman collection static button
+//[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/c2aa3aa8e5fdd8fc7e2d)
+// public link
+// https://www.getpostman.com/collections/c2aa3aa8e5fdd8fc7e2d
